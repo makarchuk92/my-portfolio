28d0dc05fe4e81f0f8d3b9aefc1c334ec58d3476
makarchuk92 <andrei.makarchuk@mail.ru>
2022-07-22 22:37:49 +0300

    Create header component and styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import CTA from './CTA'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="header">
+        <h5>Hello I'm</h5>
+        <h1>Andrei Makarchuk</h1>
+        <h5>Frontend developer and motion-designer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
