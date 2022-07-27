8d228b7ed883334e7d7b2e4a0990f4afcb78b2c3
makarchuk92 <andrei.makarchuk@mail.ru>
2022-07-28 01:16:21 +0300

    Create navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Header from '../header/Header'
+import React, { useState } from 'react'
+import './navigation.css'
+import { AiOutlineBook, AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { RiServiceLine, RiMessageLine } from 'react-icons/ri'
 
 const Navigation = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Navigation
-      
-    </div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} ><AiOutlineBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''} ><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} ><RiMessageLine /></a>
+    </nav>
   )
 }
 
